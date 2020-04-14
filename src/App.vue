8b4efdf2e11d2982bf9c349c82d83874ba0830b1
indranilokg <58342808+indranilokg@users.noreply.github.com>
2020-04-14 13:38:44 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app elevate-on-scroll>
+      <v-img
+        class="mx-2"
+        src="@/assets/okta-logo.png"
+        max-height="45"
+        max-width="45"
+        contain
+      />
+      <b>Factor API Demo</b>
+      <v-spacer />
+      <v-label v-if="authenticated">{{getUserEmail}}</v-label>
+      <v-label></v-label>
+      <v-btn class="ma-2" color="primary" to="/"
+        >Home
+        <v-icon right>mdi-home</v-icon>
+      </v-btn>
+      <v-btn
+        class="ma-2"
+        color="primary"
+        v-if="authenticated"
+        v-on:click="logout"
+        id="logout-button"
+        >Logout
+        <v-icon right>mdi-logout</v-icon>
+      </v-btn>
+      <v-btn class="ma-2" color="primary" v-else to="/login" id="login-button"
+        >Login
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  computed: {
+    authenticated: function() {
+      return this.$store.getters.isLoggedIn;
+    },
+    getUserEmail() {
+      return this.$store.getters.userEmail;
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/login");
+      });
+    }
+  }
+};
+</script>
